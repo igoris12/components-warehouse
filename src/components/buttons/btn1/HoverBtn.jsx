c78d80ee30e7021c445770ab6e62fe71dc031148
Igoris Ivanovas <igoris.x@inbox.lt>
2022-02-13 15:48:01 +0200

    hover effect done

--- src/components/buttons/btn1/HoverBtn.jsx
+++ src/components/buttons/btn1/HoverBtn.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import './HoverBtn.css';
 
 const HoverBtn = () => {
+  const hoverHandler = (e) => {
+    const btn = e.target;
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+  };
   return (
-    <a href="#" className="btn-hover">
+    <a onMouseEnter={hoverHandler} href="#" className="btn-hover">
       <span>Hover Me</span>
     </a>
   );
