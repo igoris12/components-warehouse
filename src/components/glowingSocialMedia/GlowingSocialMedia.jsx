30db1e1590854dc1d4f4a9531f58e40d4f8650e5
Igoris Ivanovas <igoris.x@inbox.lt>
2022-02-27 20:58:57 +0200

    hover animation

--- src/components/glowingSocialMedia/GlowingSocialMedia.jsx
+++ src/components/glowingSocialMedia/GlowingSocialMedia.jsx
@@ -12,27 +12,30 @@ const GlowingSocialMedia = () => {
   return (
     <ul className="socialMedia">
       <li style={{ '--clr': '#1877f2' }}>
-        <a href="#">
+        <a href="#" target="_blank">
           <BsFacebook />
         </a>
       </li>
       <li style={{ '--clr': '#00dc82' }}>
-        <a href="#">
+        <a href="#" target="_blank">
           <BsWhatsapp />
         </a>
       </li>
       <li style={{ '--clr': '#42a7ff' }}>
-        <a href="#">
+        <a
+          href="https://www.linkedin.com/in/igoris-ivanovas-839645194/"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
       </li>
       <li style={{ '--clr': '#ff0000' }}>
-        <a href="#">
+        <a href="#" target="_blank">
           <BsYoutube />
         </a>
       </li>
       <li style={{ '--clr': '#4da1f2' }}>
-        <a href="#">
+        <a href="#" target="_blank">
           <BsTwitter />
         </a>
       </li>
