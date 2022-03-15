837a0987cb85e2d6859103b714110cfef78a8b56
Igoris Ivanovas <igoris.x@inbox.lt>
2022-03-15 19:41:54 +0200

    animtaion in progress

--- src/components/thanosEffect/ThanosEffect.jsx
+++ src/components/thanosEffect/ThanosEffect.jsx
@@ -1,35 +1,153 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import ReactAnime from 'react-animejs';
 import './ThanosEffect.css';
 
 const ThanosEffect = () => {
   useEffect(() => {
     const container = document.querySelector('#thanosEffect-container');
-
-    for (let i = 0; i <= 100; i++) {
-      const block = document.createElement('div');
-      block.classList.toggle('block');
-      container.appendChild(block);
-    }
-
-    const blocks = document.querySelectorAll('.block');
-
-    // const animation = anime.timeline({
-    //   target: blocks,
-    //   easing: 'easeInOutExpo',
-    //   loop: true,
-    // });
+    // for (let i = 0; i <= 100; i++) {
+    //   const block = document.createElement('div');
+    //   block.classList.toggle('block');
+    //   container.appendChild(block);
+    // }
   }, []);
 
-  return <div className="container" id="thanosEffect-container"></div>;
+  const { Anime, stagger } = ReactAnime;
+
+  return (
+    <Anime
+      initial={[
+        {
+          targets: '.block',
+          easing: 'easeInOutExpo',
+          scale: 0,
+          loop: true,
+          translateX: function () {
+            return Math.floor(Math.random() * 360) + -360;
+          },
+          translateY: function () {
+            return Math.floor(Math.random() * 360) + -360;
+          },
+          rotate: function () {
+            return Math.floor(Math.random() * 360) + -360;
+          },
+        },
+      ]}
+    >
+      <div className="container" id="thanosEffect-container">
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+      </div>
+    </Anime>
+  );
 };
 
 export default ThanosEffect;
-{
-  /* <script
-  src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"
-  integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q=="
-  crossorigin="anonymous"
-  referrerpolicy="no-referrer"
-></script>; */
-}
