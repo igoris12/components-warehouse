3dabd6c091c897aedc97c02cb6be8d2a13993ad0
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-26 14:53:57 +0100

    gallery img layout

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Gallery.css";
+import galleryIamges from "./img/images";
 
 function Gallery() {
   return (
     <article className="grid-gallery">
-      <img src="./pic-1.webp" alt="description 1" />
-      <img src="./pic-2.webp" alt="description 2" />
-      <img src="./pic-3.webp" alt="description 3" />
-      <img src="./pic-4.webp" alt="description 4" />
-      <img src="./pic-5.webp" alt="description 5" />
-      <img src="./pic-6.webp" alt="description 6" />
-      <img src="./pic-7.webp" alt="description 7" />
-      <img src="./pic-8.webp" alt="description 8" />
+      <img src={galleryIamges[0]} alt="description 1" />
+      <img src={galleryIamges[1]} alt="description 2" />
+      <img src={galleryIamges[2]} alt="description 3" />
+      <img src={galleryIamges[3]} alt="description 4" />
+      <img src={galleryIamges[4]} alt="description 5" />
+      <img src={galleryIamges[5]} alt="description 7" />
+      <img src={galleryIamges[6]} alt="description 6" />
+      <img src={galleryIamges[7]} alt="description 8" />
     </article>
   );
 }
