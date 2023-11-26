46767432ac82a148ace953fa5c0c9a690b68af82
Igoris Ivanovas <igoris.x@inbox.lt>
2023-11-26 14:15:02 +0100

    Gallery lyout

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import './Gallery.css';
+import React from "react";
+import "./Gallery.css";
 
 function Gallery() {
   return (
-    <div className='gallery-grid'>
-      Gallsery
-    </div>
-  )
+    <article className="grid-gallery">
+      <img src="./pic-1.webp" alt="description 1" />
+      <img src="./pic-2.webp" alt="description 2" />
+      <img src="./pic-3.webp" alt="description 3" />
+      <img src="./pic-4.webp" alt="description 4" />
+      <img src="./pic-5.webp" alt="description 5" />
+      <img src="./pic-6.webp" alt="description 6" />
+      <img src="./pic-7.webp" alt="description 7" />
+      <img src="./pic-8.webp" alt="description 8" />
+    </article>
+  );
 }
 
-export default Gallery
+export default Gallery;
